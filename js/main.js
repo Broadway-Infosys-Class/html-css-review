13599fb75a55f6253f18fb8a8e7f448f87e9f24e
headaashish <aashdahal@gmail.com>
2021-04-05 13:10:07 +0545

    completes calculator code using jQuery

--- js/main.js
+++ js/main.js
@@ -1,8 +1,28 @@
 $(function(){
-
+    var operator = 0;
     $(".number").click(function(){
+        if ($(this).hasClass("operator")){
         $("#result").val($("#result").val()+$(this).text());
-    });
+        } else if ($(this).hasClass("calc")){
+            operator++;
+            if (operator == 1){
+                $("#result").val($("#result").val()+$(this).text());
+            } else {
+            $("#result").val(eval($("#result").val())+$(this).text());
+            }
+        }
+        else if ($(this).hasClass("clear")) {
+            $("#result").val("");
+            operator = 0;
+        }
+        else if ($(this).hasClass("equals")) {
+            $("#result").val(eval($("#result").val()));
+            operator = 0;
+        }
+        else {
+            alert('wrong input');
+        }
+     });
   
   });
   
